061d858a4234f79eccf37da7feb77f28103a66a9
carlos <carloshsf3899@gmail.com>
2023-11-29 16:13:46 +0000

    Add IntelliJ template

--- src/messages/Serializable.java
+++
@@ -1,8 +0,0 @@
-package messages;
-
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-public interface Serializable {
-    void serialize(DataOutputStream out) throws IOException;
-}
