889e977a63fcd25c2561a1390020351f406ea32d
CarlosHSF <44176623+CarlosHSF99@users.noreply.github.com>
2023-12-20 00:11:03 +0000

    Jobs (#7)
    * Add Auth class to handle authentication requests concurrenty
    
    * Make server session code concurrent and move it to Session class
    
    * Move Client authentication code to authenticate method
    
    * Change type of lock to ReadWriteLock
    
    * Add job execution in server
    
    * Add job execution request to client
    
    * Update SD.iml with the sd23.jar dependency
    
    * Remove unrelated class

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,13 +1,14 @@
 package client;
 
-import messages.AuthReply;
-import messages.AuthRequest;
-import messages.Type;
+import messages.*;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -17,9 +18,20 @@ public class Client {
              var in = new DataInputStream(socket.getInputStream());
              var out = new DataOutputStream(socket.getOutputStream())) {
             authenticate(in, out);
+
+            while (true) {
+                send(out);
+                Thread.startVirtualThread(() -> {
+                    try {
+                        receive(in);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
         } catch (IOException | NoSuchElementException e) {
             // throw new RuntimeException(e);
-            System.out.println("Failed to connect to server.");
+            System.out.println("Connection ended.");
         }
     }
 
@@ -34,7 +46,7 @@ public class Client {
 
             new AuthRequest(username, password).serialize(out);
 
-            while (Type.deserialize(in) != messages.Type.AUTH_REPLY) {
+            while (Type.deserialize(in) != Type.AUTH_REPLY) {
                 in.readAllBytes();
             }
 
@@ -45,6 +57,37 @@ public class Client {
             } else {
                 System.out.println("Failed to login.");
             }
+        }
+    }
+
+    private static void send(DataOutputStream out) throws IOException {
+        var scanner = new Scanner(System.in);
+        var line = scanner.nextLine();
+        var tokens = line.split(" ");
+
+        switch (tokens[0]) {
+            case "exec" -> {
+                var job = Files.readAllBytes(Path.of(tokens[1]));
+                new JobRequest(job).serialize(out);
+            }
+            default -> System.out.println("Unknown command");
+        }
+    }
+
+    private static void receive(DataInputStream in) throws IOException {
+        switch (Type.deserialize(in)) {
+            case JOB_REPLY_OK -> {
+                var jobReplyOk = JobReplyOk.deserialize(in);
+                var fileOut = "here.txt";
+                try (var fos = new FileOutputStream(fileOut)) {
+                    fos.write(jobReplyOk.output());
+                }
+            }
+            case JOB_REPLY_ERROR -> {
+                var jobReplyError = JobReplyError.deserialize(in);
+                System.out.println("Job failed.\n\tCode: " + jobReplyError.code() + "\n\tMessage: " + jobReplyError.message());
+            }
+            default -> System.out.println("Received unknown message type");
         }
     }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,14 +1,7 @@
 package server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.util.HashMap;
-
-import messages.AuthReply;
-import messages.AuthRequest;
-import messages.Type;
 
 public class Server {
     public static void main(String[] args) throws IOException {
