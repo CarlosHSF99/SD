4d2ff3b3cda56da4167b88eeb05976b8388551e0
carlos <carloshsf3899@gmail.com>
2023-12-25 04:16:08 +0000

    Remove option to run the job in the server

--- src/server/MasterScheduler.java
+++ src/server/MasterScheduler.java
@@ -1,29 +1,20 @@
 package server;
 
 import connection.messages.JobReplyError;
-import connection.messages.JobReplyOk;
 import connection.messages.JobRequest;
 import connection.multiplexer.TaggedConnection;
 import connection.utils.Message;
-import sd23.JobFunctionException;
-import worker.Scheduler;
 
 import java.io.IOException;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MasterScheduler {
-    private final worker.Scheduler localScheduler;
     private final WorkerPool workerPool = new WorkerPool();
     // maybe discriminate lock for availableMemory and pendingJobs
     private final Lock lock = new ReentrantLock();
     private int pendingJobs = 0;
-    private int availableMemory;
-
-    public MasterScheduler(int memory) {
-        this.localScheduler = new Scheduler(memory);
-        this.availableMemory = memory;
-    }
+    private int availableMemory = 0;
 
     public void addWorker(TaggedConnection connection, int memory) {
         lock.lock();
@@ -53,7 +44,7 @@ public class MasterScheduler {
                 try {
                     workerLink = workerPool.getWorker(jobRequest.memory());
                 } catch (NoSuitableWorkerException e) {
-                    return runJobLocally(jobRequest);
+                    return new JobReplyError(0, "Not enough memory");
                 }
                 try {
                     System.out.println("Running job remotely");
@@ -75,17 +66,6 @@ public class MasterScheduler {
             } finally {
                 lock.unlock();
             }
-        }
-    }
-
-    private Message runJobLocally(JobRequest jobRequest) throws InterruptedException {
-        System.out.println("Running job locally");
-        try {
-            return new JobReplyOk(localScheduler.addJob(jobRequest));
-        } catch (JobTooBigException e) {
-            return new JobReplyError(0, "Not enough memory");
-        } catch (JobFunctionException e) {
-            return new JobReplyError(e.getCode(), e.getMessage());
         }
     }
 
